7be2ee837e921762f0831e162cfdc0783977d164
tienpmath <designdalat@gmail.com>
2024-12-07 21:13:01 +0700

    remove teant

--- src/app/auth/login/route.ts
+++ src/app/auth/login/route.ts
@@ -1,14 +1,14 @@
 import { clientConfig } from '@/config'
 import { getSession } from '@/lib/actions'
-import {getClientConfig} from '@/lib/session-utils'
+import { getClientConfig } from '@/lib/session-utils'
 import * as client from 'openid-client'
 /**
  * Handles the GET request for the login route.
- * 
+ *
  * This function initiates the PKCE (Proof Key for Code Exchange) flow for OAuth2 authentication.
  * It generates a code verifier and code challenge, constructs the authorization URL with the necessary parameters,
  * and redirects the user to the authorization endpoint.
- * 
+ *
  * @returns {Promise<Response>} A promise that resolves to a Response object that redirects the user to the authorization URL.
  */
 export async function GET() {
@@ -17,11 +17,11 @@ export async function GET() {
   let code_challenge = await client.calculatePKCECodeChallenge(code_verifier)
   const openIdClientConfig = await getClientConfig()
   let parameters: Record<string, string> = {
-    "redirect_uri": clientConfig.redirect_uri,
-    "scope": clientConfig.scope!,
+    redirect_uri: clientConfig.redirect_uri,
+    scope: clientConfig.scope!,
     code_challenge,
-    "code_challenge_method": clientConfig.code_challenge_method,
-    "__tenant": session.tenantId === 'default' ? "" : session.tenantId!,
+    code_challenge_method: clientConfig.code_challenge_method,
+    //"__tenant": session.tenantId === 'default' ? "" : session.tenantId!,
   }
   let state!: string
   if (!openIdClientConfig.serverMetadata().supportsPKCE()) {
